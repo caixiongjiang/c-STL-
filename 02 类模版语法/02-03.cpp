349ff5141d00b816ad0b47cc0796627d0a635b24
caixiongjiang <1837278011@qq.com>
2021-12-06 10:58:01 +0800

    2021 12.06

--- 02 类模版语法/02-03.cpp
+++ 02 类模版语法/02-03.cpp
@@ -5,10 +5,50 @@
 #include <iostream>
 using namespace std;
 
+//类模版中成员函数创建的时机
+//类模版成员函数在调用时才去创建
+
+class Person1
+{
+public:
+    void showPerson1()
+    {
+        cout << "Person1 show" << endl;
+    }
+};
+
+class Person2
+{
+public:
+    void showPerson2()
+    {
+        cout << "Person2 show" << endl;
+    }
+};
+//没有调用之前不会报错
+
+template<class T>
+class Myclass
+{
+public:
+    T obj;
+    //类模版中的成员函数
+    void func1()
+    {
+        obj.showPerson1();
+    }
+    void func2()
+    {
+        obj.showPerson2();
+    }
+};
 
 void test01()
 {
-
+    Myclass<Person1> m;
+    //以下两段代码只能运行一段
+    m.func1();
+    //m.func2();
 }
 
 int main()
