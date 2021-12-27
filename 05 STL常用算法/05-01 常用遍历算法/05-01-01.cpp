35acf73aecb965b6ed1df8364ca306af4ff7f986
caixiongjiang <1837278011@qq.com>
2021-12-27 14:49:43 +0800

    2021 12.27

---
+++ 05 STL常用算法/05-01 常用遍历算法/05-01-01.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Jarson's mac on 2021/12/27.
+//
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+
+//常用遍历算法
+
+//普通函数
+void print01(int val)
+{
+    cout << val << " ";
+}
+
+//仿函数
+class print02
+{
+public:
+    void operator()(int val)
+    {
+        cout << val << " ";
+    }
+};
+
+void test01()
+{
+    vector<int> v;
+    for(int i = 0; i < 10; i++)
+    {
+        v.push_back(i);
+    }
+    //普通函数在遍历时要放入函数名
+    for_each(v.begin(), v.end(), print01);
+    cout << endl;
+    //仿函数在遍历时需要放入匿名函数对象
+    for_each(v.begin(), v.end(), print02());
+}
+
+int main()
+{
+    test01();
+    return 0;
+}
